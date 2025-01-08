a2fc274674cda5c7bbc209a29759982fb129c8c8
Chandlar Lanier <cklanier@outlook.com>
2025-01-07 22:19:12 -0500

    Correct mistakes of character recognition software

--- 8ku.js
+++ 8ku.js
@@ -1,4 +1,4 @@
-/* Fundamentals: Return
+/* 1. Fundamentals: Return
   Description: 
     Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
@@ -21,7 +21,7 @@
     const subt = (a, b) => a - b;
 */
 
-/* What's the real floor?
+/* 2. What's the real floor?
   Description:
     Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
     Write a function that given a floor in the american system returns the floor in the european system.
@@ -48,4 +48,21 @@
     
   Best Solution: 
     const getRealFloor = (n) => n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+*/
+
+/* 3. Correct the mistakes of the character recognition software
+  Description: 
+    Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+    When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+    Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+      S is misinterpreted as 5
+      O is misinterpreted as 0
+      I is misinterpreted as 1
+
+    The test cases contain numbers only by mistake.
+
+    My Solution:
+      const correct = string => string.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I");
+
 */
