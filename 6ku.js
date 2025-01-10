0031f05ea777d0e77f7bfcac5add12e296e00777
Chandlar Lanier <cklanier@outlook.com>
2025-01-09 21:56:16 -0500

    Consonant value

--- 6ku.js
+++ 6ku.js
@@ -23,5 +23,40 @@
         }
         return result;
       }
+*/
 
+/* 2. Consonant value
+  Description:
+    Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+    We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+    For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia c"
+      "zodiac" -> 26
+      The consonant substrings are: "z", "d" and "c" with values "z" = 26, "d" = 4 and "c" = 3. The highest is 26.
+
+      "strength" -> 57
+      The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+
+  My Solution: 
+    function solve(s) {
+      let sArray = s.toLowerCase().split("");
+      sArray = sArray.map(char => /[aeiouAEIOU]/.test(char) ? ' ' : char);
+
+      let value = 0;
+      let values = [];
+      for (let i = 0; i < sArray.length; i++) {
+        if (sArray[i] === " ") {
+          continue;
+        }
+
+        value += sArray[i].charCodeAt(0) - 96;
+
+        if (sArray[i+1] === " " || i === sArray.length - 1) {
+          values.push(value);
+          value = 0;
+        }  
+      }
+
+      return Math.max(...values);
+    };
 */
