c0019abd59f8edb37d3ed39a125b653b7fdfba09
Chandlar Lanier <cklanier@outlook.com>
2025-01-10 19:14:19 -0500

    Simple encryption #1 - alternating split

--- 6ku.js
+++ 6ku.js
@@ -60,3 +60,70 @@
       return Math.max(...values);
     };
 */
+
+/* 3. Simple Encryption #1 - Alternating Split
+
+  Description:
+    Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+    Examples:
+      encrypt("012345", 1)  =>  "135024"
+      encrypt("012345", 2)  =>  "135024"  ->  "304152"
+      encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+      encrypt("01234", 1)  =>  "13024"
+      encrypt("01234", 2)  =>  "13024"  ->  "32104"
+      encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+
+    Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+    If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+  My Solution:
+    function encrypt(text, n) {
+      if (text === "" || n < 0) return text;
+  
+      let newText = text;
+  
+      for (let i = 0; i < n; i++) {
+        let odds = [];
+        let evens = [];
+    
+        for (let j = 0; j < newText.length; j++) {
+          if (j % 2 === 0) {
+            evens.push(newText[j])
+          } else {
+            odds.push(newText[j])
+          }
+        }
+    
+        newText = odds.join("") + evens.join("");
+      }
+      return newText;
+    }
+
+    function decrypt(encryptedText, n) {
+      if (encryptedText === "" || n < 0) return encryptedText;
+  
+      let newText = encryptedText;
+  
+      for (let i = 0; i < n; i++) {
+        const half = Math.floor(newText.length / 2);
+        const odds = newText.slice(0, half).split("");
+        const evens = newText.slice(half).split("");
+        const mergedHalves = [];
+    
+        for (let j = 0; j < newText.length; j++) {
+          if (j % 2 === 0) {
+            mergedHalves.push(evens.shift());
+          } else {
+            mergedHalves.push(odds.shift());
+          }
+        }
+    
+        newText = mergedHalves.join("");
+      }
+  
+      return newText;
+    }
+*/
